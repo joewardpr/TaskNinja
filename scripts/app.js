e3085158056df639165348d1980caf80195006d0
= <=>
2015-11-04 21:23:12 -0400

    Task 1

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,25 @@
 
 var app = angular
   .module('TaskNinjaApp', [
-    'ngAnimate',
-    'ngResource',    
+    'ngAnimate',    
+    'ngResource',
     'ngRoute',    
     'firebase'
   ])
-  .constant('FURL', 'https://your-firebase.firebaseio.com/')  
+  .constant('FURL', 'https://taskninjajoewardpr.firebaseio.com/')  
   .config(function ($routeProvider) {
     $routeProvider      
       .when('/', {
-        templateUrl: 'views/main.html'        
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'     
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
+      })
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',
+        controller: 'TaskController'
       })
       .otherwise({
         redirectTo: '/'
